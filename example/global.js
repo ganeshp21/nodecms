036c0ed1ae2fd3fca383fdc07f94a745429d1b21
ganeshp21 <48122262+ganeshp21@users.noreply.github.com>
2020-08-29 18:53:57 +0530

    example global changes

--- example/global.js
+++ example/global.js
@@ -1,5 +1,11 @@
+var path = require('path');
 var name = 'Ganesh Papde'; 
 
 var test = name.toUpperCase(name); 
-console.log(test);
+//console.log(test); 
 
+console.log(`your name is ${test}`);
+
+console.log(__dirname);
+console.log(__filename);
+console.log(path.basename(__filename));
